fc6124c566134375075c75cb00e9694509e25537
Johanderson <johander1822@gmail.com>
2024-09-19 18:18:27 +0200

    some docs

--- lib/llm_composer.ex
+++ lib/llm_composer.ex
@@ -1,5 +1,9 @@
 defmodule LlmComposer do
-  @moduledoc false
+  @moduledoc """
+  `LlmComposer` is responsible for interacting with a language model to perform chat-related operations,
+  such as running completions and executing functions based on the responses. The module provides 
+  functionality to handle user messages, generate responses, and automatically execute functions as needed.
+  """
 
   alias LlmComposer.Helpers
   alias LlmComposer.LlmResponse
@@ -10,6 +14,16 @@ defmodule LlmComposer do
 
   @type messages :: [Message.t()]
 
+  @doc """
+  Initiates a simple chat interaction with the language model.
+
+  ## Parameters
+    - `settings`: The settings for the language model, including prompts and options.
+    - `msg`: The user message to be sent to the language model.
+
+  ## Returns
+    - The result of the language model's response, which may include function executions if specified.
+  """
   @spec simple_chat(Settings.t(), String.t()) :: Helpers.action_result()
   def simple_chat(%Settings{} = settings, msg) do
     messages = get_messages(settings, msg, [], %{})
@@ -17,6 +31,17 @@ defmodule LlmComposer do
     run_completion(settings, messages)
   end
 
+  @doc """
+  Runs the completion process by sending messages to the language model and handling the response.
+
+  ## Parameters
+    - `settings`: The settings for the language model, including prompts, model options, and functions.
+    - `messages`: The list of messages to be sent to the language model.
+    - `previous_response` (optional): The previous response object, if any, used for context.
+
+  ## Returns
+    - A tuple containing `:ok` with the response or `:error` if the model call fails.
+  """
   @spec run_completion(Settings.t(), messages(), LlmResponse.t() | nil) :: Helpers.action_result()
   def run_completion(settings, messages, previous_response \\ nil) do
     system_msg = Message.new(:system, settings.system_prompt)
@@ -43,6 +68,7 @@ defmodule LlmComposer do
     end)
   end
 
+  @doc false
   @spec get_messages(Settings.t(), String.t(), messages(), map()) :: messages()
   defp get_messages(settings, current_message, old_messages, opts) do
     old_messages ++ [Message.new(:user, user_prompt(settings, current_message, opts))]
@@ -55,6 +81,7 @@ defmodule LlmComposer do
     prompt <> message
   end
 
+  @doc false
   @spec maybe_run_functions(LlmResponse.t(), messages(), Settings.t()) :: Helpers.action_result()
   defp maybe_run_functions(res, messages, settings) do
     res
