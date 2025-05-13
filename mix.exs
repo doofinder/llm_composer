91a6d78fad22ba1206ca429622ce933e22d3953d
Moisès Macià <moises@doofinder.com>
2025-05-13 12:34:09 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LlmComposer.MixProject do
   def project do
     [
       app: :llm_composer,
-      version: "0.3.6",
+      version: "0.4.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
