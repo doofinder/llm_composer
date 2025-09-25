82dfc22ba60646deffd64f68ccddf97dd56f6064
Johanderson <johander1822@gmail.com>
2025-09-25 16:49:04 +0200

    removed bad comment

--- test/llm_composer_test.exs
+++ test/llm_composer_test.exs
@@ -1,8 +1,4 @@
 defmodule LlmComposerTest do
   use ExUnit.Case, async: true
   doctest LlmComposer
-
-  # Since most functions in LlmComposer require provider mocking for full testing,
-  # we'll focus on doctests and basic functionality for now.
-  # Integration tests with mocked providers will come later.
 end
