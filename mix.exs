efe4cc34b85307bc90d76b865253afea6e03013c
Johanderson <johander1822@gmail.com>
2025-11-06 14:38:24 +0100

    release version 0.12.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LlmComposer.MixProject do
   def project do
     [
       app: :llm_composer,
-      version: "0.12.1",
+      version: "0.12.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
